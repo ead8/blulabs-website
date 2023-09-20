50c100bd4f258043c91b50e49c5538b5c55c717d
ead8 <mststy8@gmail.com>
2023-09-20 23:50:43 +0300

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,30 @@
-import { motion } from "framer-motion";
 import { about } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
+import { useRef } from "react";
 
-const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row justify-center items-center p-6 rounded-[20px] ${index !== about.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+const FeatureCard = ({ icon, title, content, index }) => {
+
+  return (
+
+    <div className={`flex flex-row justify-center items-center p-6 rounded-[20px] ${index !== about.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+      </div>
+      <div className="flex-1 flex flex-col ml-3 ">
+        <h4 className="font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+          {title}
+        </h4>
+        <p className="font-normal text-dimWhite text-[16px] leading-[24px]">
+          {content}
+        </p>
+      </div>
     </div>
-    <div className="flex-1 flex flex-col ml-3 ">
-      <h4 className="font-semibold text-white text-[18px] leading-[23.4px] mb-1">
-        {title}
-      </h4>
-      <p className="font-normal text-dimWhite text-[16px] leading-[24px]">
-        {content}
-      </p>
-    </div>
-  </div>
-);
+  );
+}
 
 const About = () => (
-  <motion.div>
+  <div>
     <section id="about" className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>
@@ -39,7 +43,7 @@ const About = () => (
         ))}
       </div>
     </section>
-  </motion.div>
+  </div>
 
 );
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,33 +1,39 @@
 import styles, { layout } from "../style";
 import { services } from "../constants";
 
+import { motion } from "framer-motion";
+
 const Services = () => (
- <section id="services">
-  {
-    services.map(({icon,title,content})=>(
-      <div id="title" className={layout.sectionReverse} key={title}>
-      <div className={layout.sectionImgReverse}>
-        <img src={icon} alt="Services" className="w-[100%] h-[80%] relative z-[5]" />
-  
-        {/* gradient start */}
-        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
-        {/* gradient end */}
-      </div>
-  
-      <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>
-          {title}
-        </h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-         {content}
-        </p>
-  
-      </div>
-    </div>
-    ))
-  }
- </section>
+  <section id="services">
+    {
+      services.map(({ icon, title, content }) => (
+        <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+          id="title" className={layout.sectionReverse} key={title}>
+          <div className={layout.sectionImgReverse}>
+            <img src={icon} alt="Services" className="w-[100%] h-[80%] relative z-[5]" />
+
+            {/* gradient start */}
+            <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+            <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+            {/* gradient end */}
+          </div>
+
+          <div className={layout.sectionInfo}>
+            <h2 className={styles.heading2}>
+              {title}
+            </h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+              {content}
+            </p>
+
+          </div>
+        </motion.div>
+      ))
+    }
+  </section>
 );
 
 export default Services;
